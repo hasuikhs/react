609b8fdffe553c4eb44ecf79acd331b71032c7d3
Hanseok <hasuikhs@gmail.com>
2020-11-13 18:26:23 +0900

    201113|add react img

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -3,11 +3,28 @@ import './App.css';
 import { Component } from 'react';
 import Movie from './Movie';
 
+const movieTitles = [
+  'spring',
+  'summer',
+  'autumn',
+  'winter'
+];
+
+const movieImages = [
+  'https://img.khan.co.kr/news/2020/04/11/2020041001001383400107401.jpg',
+  'https://s3-eu-west-1.amazonaws.com/eflanguagesblog/wp-content/uploads/sites/57/2019/06/17094434/photo-1507525428034-b723cf961d3e.jpg',
+  'https://www.greentrust.or.kr/wp-content/uploads/2017/10/fall-1072821_1280-1024x682.jpg',
+  'https://img.hankyung.com/photo/202001/99.21354244.1.jpg'
+]
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie />
+        <Movie title={movieTitles[0]} image={movieImages[0]} />
+        <Movie title={movieTitles[1]} image={movieImages[1]} />
+        <Movie title={movieTitles[2]} image={movieImages[2]} />
+        <Movie title={movieTitles[3]} image={movieImages[3]} />
       </div>
     );
   }
--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -5,8 +5,8 @@ class Movie extends Component {
     render() { 
         return (
             <div>
-                <MoviePoster />
-                <h1>hello this is a movie</h1>
+                <MoviePoster image={this.props.image} />
+                <h1>{this.props.title}</h1>
             </div>
         )
     }
@@ -14,8 +14,9 @@ class Movie extends Component {
 
 class MoviePoster extends Component {
     render() {
+        console.log(this.props)
         return (
-            <img src="https://upload.wikimedia.org/wikipedia/ko/thumb/1/18/%EC%96%B4%EB%B2%A4%EC%A0%B8%EC%8A%A4.jpg/220px-%EC%96%B4%EB%B2%A4%EC%A0%B8%EC%8A%A4.jpg"/>
+            <img width="400" src={this.props.image}/>
         )
     }
 }
