7d07b1e8ba15369e2ee732f902fb191c9bfdbbd3
Hanseok <hasuikhs@gmail.com>
2020-11-16 23:15:25 +0900

    201116|Update functional component

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -5,29 +5,6 @@ import Season from './Season';
 class App extends Component {
 
   state = {
-    greeting: 'Hello',
-    seasons: [
-      {
-        id: 0,
-        title: 'spring',
-        img: 'https://img.khan.co.kr/news/2020/04/11/2020041001001383400107401.jpg'
-      },
-      {
-        id: 1,
-        title: 'summer',
-        img: 'https://s3-eu-west-1.amazonaws.com/eflanguagesblog/wp-content/uploads/sites/57/2019/06/17094434/photo-1507525428034-b723cf961d3e.jpg'
-      },
-      {
-        id: 2,
-        title: 'autumn',
-        img: 'https://www.greentrust.or.kr/wp-content/uploads/2017/10/fall-1072821_1280-1024x682.jpg'
-      },
-      {
-        id: 3,
-        title: 'winter',
-        img: 'https://img.hankyung.com/photo/202001/99.21354244.1.jpg'
-      }
-    ]
   };
 
   componentDidMount() {
@@ -35,10 +12,23 @@ class App extends Component {
     setTimeout(() => {
       this.setState({
         seasons: [
-          // infinite scroll
-          ...this.state.seasons, // 이 코드를 뒤에 배치할 경우 새롭게 추가되는 사진을 앞으로 붙이기 가능
           {
-            id: 4,
+            title: 'spring',
+            img: 'https://img.khan.co.kr/news/2020/04/11/2020041001001383400107401.jpg'
+          },
+          {
+            title: 'summer',
+            img: 'https://s3-eu-west-1.amazonaws.com/eflanguagesblog/wp-content/uploads/sites/57/2019/06/17094434/photo-1507525428034-b723cf961d3e.jpg'
+          },
+          {
+            title: 'autumn',
+            img: 'https://www.greentrust.or.kr/wp-content/uploads/2017/10/fall-1072821_1280-1024x682.jpg'
+          },
+          {
+            title: 'winter',
+            img: 'https://img.hankyung.com/photo/202001/99.21354244.1.jpg'
+          },
+          {
             title: 'destroy',
             img: 'https://img.huffingtonpost.com/asset/5d8117463b0000c49fd5fbf7.jpeg?ops=1200_630'
           }
@@ -47,13 +37,17 @@ class App extends Component {
     }, 2000)
   }
 
+  _renderSeasons = () => {
+    const seasons = this.state.seasons.map((season, index) => {
+      return <Season title={season.title} img={season.img} key={index} />
+    })
+    return seasons;
+  }
+
   render() {
     return (
       <div className="App">
-        {this.state.greeting}
-        {this.state.seasons.map(season => {
-          return <Season title={season.title} img={season.img} key={season.id} />
-        })}
+        {this.state.seasons ? this._renderSeasons() : 'Loading'}
       </div>
     );
   }
--- movie_app/src/Season.js
+++ movie_app/src/Season.js
@@ -1,35 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Season.css';
 
-class Season extends Component {
-
-    static propTypes = {
-        title:PropTypes.string.isRequired,  // 꼭 필요한 인자인 경우 isRequired
-        img: PropTypes.string.isRequired
-    }
-
-    render() { 
-        return (
-            <div>
-                <SeasonImage img={this.props.img} />
-                <h1>{this.props.title}</h1>
-            </div>
-        )
-    }
+function Season({ title, img }) {
+    return (
+        <div>
+            <SeasonImage img={img} />
+            <h1>{title}</h1>
+        </div>
+    )
 }
 
-class SeasonImage extends Component {
+function SeasonImage({ img }) {
+    return (
+        <img width="400" src={img} alt="season image" />
+    )
+}
 
-    static propTypes = {
-        img: PropTypes.string.isRequired
-    }
+Season.prototype = {
+    title: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired
+}
 
-    render() {
-        return (
-            <img width="400" src={this.props.img} alt="season image" />
-        )
-    }
+SeasonImage.prototype = {
+    img: PropTypes.string.isRequired
 }
 
 export default Season;
