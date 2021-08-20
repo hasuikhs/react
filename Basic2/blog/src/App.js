4e825f036d8548b01e828cc66be5240a27cf7155
Hanseok <hasuikhs@gmail.com>
2021-08-20 23:33:07 +0900

    210820|Update react state

--- Basic2/blog/src/App.js
+++ Basic2/blog/src/App.js
@@ -1,16 +1,40 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  let posts = '테스트 글';
+  let [title, setTitle] = useState(['test1', 'test2', 'test3']);
+  let [likeCnt, setLikeCnt] = useState(0);
+
+  function changeTitle() {
+    let newArr = [...title];  // array deep copy
+    newArr.sort().reverse();
+
+    setTitle( newArr );
+  }
 
   return (
     <div className="App">
       <div className="black-nav">
-        <div style={ { color: 'yellow', fontSize: '30px' } }>Test Blog</div>
+        <div style={ { color: 'beige', fontSize: '30px' } }>Test Blog</div>
       </div>
-      <h4>{ posts }</h4>
+      <button onClick={ changeTitle }>button</button>
+      <div className="list">
+        <h4>{ title[0] } <span onClick={ () => { setLikeCnt(++likeCnt) } }>👍</span> { likeCnt } </h4>  
+        <p>8월 20일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h4>{ title[1] }</h4>
+        <p>8월 20일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h4>{ title[2] }</h4>
+        <p>8월 20일 발행</p>
+        <hr/>
+      </div>
     </div>
   );
 }
