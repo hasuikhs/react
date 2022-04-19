6fb5926c33bf6c51cc9e52a0a5aa5a562c8a2297
hanseok kim <hasuikhs@gmail.com>
2022-04-19 22:20:06 +0900

    220419|Update react textarea

--- test_prj/src/App.js
+++ test_prj/src/App.js
@@ -5,10 +5,19 @@ function App() {
   const [title, setTitle] = useState('');
   const titleRef = useRef(null);
 
-  const submit = () => {
+  const [body, setBody] = useState('');
+  const bodyRef = useRef(null);
+
+  const onSubmit = () => {
     if (title === '') {
       alert('title 열이 비어있음!');
       titleRef.current.focus();
+      return;
+    }
+
+    if (body ===  '') {
+      alert('body 내용이 비어있음!');
+      bodyRef.current.focus();
       return;
     }
     console.log('submit')
@@ -25,8 +34,18 @@ function App() {
           value={ title }
           onChange={ e => setTitle(e.target.value) }
         />
+      </div><div className="mb-3">
+        <label className="form-label">Body</label>
+        <textarea
+          ref={ bodyRef }
+          className="form-control"
+          placeholder="body를 입력해주세요"
+          value={ body }
+          rows="20"
+          onChange={ e => setBody(e.target.value) }
+        />
       </div>
-      <button className="btn btn-primary" onClick={ submit }>
+      <button className="btn btn-primary" onClick={ onSubmit }>
         Post
       </button>
     </div>
