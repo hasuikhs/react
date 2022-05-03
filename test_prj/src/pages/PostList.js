87bf658d17a8fb20703b1dd4268856d272eab3a4
Hanseok <hasuikhs@gmail.com>
2022-05-03 22:39:58 +0900

    220503|Update react table

--- test_prj/src/pages/PostList.js
+++ test_prj/src/pages/PostList.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Table } from 'react-bootstrap';
 import API from '../common/API';
 
 function PostList() {
@@ -8,13 +8,13 @@ function PostList() {
   const getPosts = async () => {
     try {
       const res = await API.get('/posts');
-      
+
       if (res.status === 200) {
         setPosts(res.data);
       }
-      
+
     } catch (error) {
-      
+
     }
   }
 
@@ -24,14 +24,28 @@ function PostList() {
 
   return (
     <Container>
-      {posts.map(item => {
-        return (
-          <>
-            <p>title</p> {item.title}
-            <p>body</p> {item.body}
-          </>
-        )
-      })}
+      <Row className='mt-5'>
+        <Table striped bordered hover responsive>
+          <thead>
+            <tr>
+              <th width="50px">#</th>
+              <th>title</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              posts.map(item => {
+                return (
+                  <tr>
+                    <td>{item.id}</td>
+                    <td>{item.title}</td>
+                  </tr>
+                );
+              })
+            }
+          </tbody>
+        </Table>
+      </Row>
     </Container>
   );
 }
