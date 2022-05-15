492d0de64459001a9830de09befc9d9c12de979f
hanseok kim <hasuikhs@gmail.com>
2022-05-15 13:59:12 +0900

    220515|Update paginsation ellipsis

--- test_prj/src/components/TablePagination.js
+++ test_prj/src/components/TablePagination.js
@@ -5,6 +5,8 @@ function TablePagination({page, totalCount, limit, setPage}) {
 
   // 한번에 5개의 페이지를 보여주고 싶음
   const pages = [];
+  const prevEllipsis = [];
+  const nextEllipsis = [];
 
   let endPage = parseInt(totalCount / limit);
   if (totalCount > limit * endPage) {
@@ -25,11 +27,25 @@ function TablePagination({page, totalCount, limit, setPage}) {
     }
   }
 
+  if (leftSide !== 1) {
+    prevEllipsis.push(leftSide - 1);
+  }
+
+  if (rightSide !== endPage) {
+    nextEllipsis.push(rightSide + 1);
+  }
+
   return (
     <>
       <Pagination className='justify-content-md-center'>
         <Pagination.First onClick={ () => setPage(1)} disabled={ page === 1 }/>
         <Pagination.Prev onClick={ () => setPage(page - 1) } disabled={ page === 1 } />
+        { prevEllipsis.map(prev => (
+            <Pagination.Ellipsis
+              key={ prev }
+              onClick={ () => setPage(prev) }
+            />
+        )) }
         { pages.map(curPage => (
           <Pagination.Item
             key={ curPage }
@@ -38,6 +54,12 @@ function TablePagination({page, totalCount, limit, setPage}) {
           >
             { curPage }
           </Pagination.Item>
+        )) }
+        { nextEllipsis.map(next => (
+            <Pagination.Ellipsis
+              key={ next }
+              onClick={ () => setPage(next) }
+            />
         )) }
         <Pagination.Next onClick={ () => setPage(page + 1) } disabled={ page === endPage } />
         <Pagination.Last onClick={ () => setPage(endPage) } disabled={ page === endPage } />
