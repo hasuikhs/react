5738e913354f62aacbd9258849d7543cfa33ff75
hanseok kim <hasuikhs@gmail.com>
2023-01-09 00:41:30 +0900

    230109|Modify useResizeObserver

--- demo/src/hooks/useResizeObserver.js
+++ demo/src/hooks/useResizeObserver.js
@@ -1,56 +1,33 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
 
-// const useInfiniteScroll = targetEl => {
-//   const observerRef = useRef(null);
-//   const [intersecting, setIntersecting] = useState(false);
-
-//   const getObserver = useCallback(() => {
-//     if (!observerRef.current) {
-//       observerRef.current = new IntersectionObserver(entries =>
-//         setIntersecting(entries.some(entry => entry.isIntersecting))
-//       );
-//     }
-
-//     return observerRef.current;
-//   }, [ observerRef.current ]);
-
-//   useEffect(() => {
-//     if (targetEl.current) getObserver().observe(targetEl.current);
-
-//     return () => {
-//       getObserver().disconnect();
-//     }
-//   }, [ targetEl.current ]);
-
-//   return intersecting;
-// }
-
-// export default useInfiniteScroll;
-
 const useResizeObserver = targetEl => {
+  console.log('init', targetEl)
   const observerRef = useRef(null);
 
   const [width, setWidth] = useState(0);
   const [height, setHeight] = useState(0);
 
   const getObserver = useCallback(() => {
+    console.log(observerRef)
     if (!observerRef.current) {
       observerRef.current = new ResizeObserver(entries => {
-        const { contentRect } = entries.find(entry => entry.target === targetEl.current);
+        const target = entries.find(entry => entry.target === targetEl.current);
 
-        setWidth(contentRect.width);
-        setHeight(contentRect.height);
+        if (target) {
+          setWidth(target.contentRect.width);
+          setHeight(target.contentRect.height);
+        }
       });
     }
 
     return observerRef.current;
-  }, [ observerRef.current ]);
+  }, [ targetEl ]);
 
   useEffect(() => {
     if (targetEl.current) getObserver().observe(targetEl.current);
 
     return () => getObserver().disconnect();
-  });
+  }, [ ]);
 
   return [width, height];
 }
