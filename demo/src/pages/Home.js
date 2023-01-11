a37822edea9340832c3ce55331f3e2e4d7d00a9d
hanseok kim <hasuikhs@gmail.com>
2023-01-11 23:17:36 +0900

    230111|Update home style

--- demo/src/pages/Home.js
+++ demo/src/pages/Home.js
@@ -3,32 +3,59 @@ import styled from 'styled-components';
 
 import routes from '../routes';
 
-const MainDiv = styled.div`
+const MainContainer = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  margin: 10px;
+  width: 900px;
+`;
+
+const Card = styled.div`
+  width: 160px;
+  height: 200px;
+  border: 1px solid black;
+  border-radius: 20px;
+  box-shadow: 1px;
+  margin: 10px;
+  padding: 20px;
+  transition: 0.7s;
+  &: hover {
+    background-color: #E3F2FD;
+    border: 1px solid #90CAF9;
+  }
 `;
 
 function Home() {
   return (
-    <MainDiv>
-      연습장
-      {
-        routes.map(route => {
-          return !route.children
-                  ? <Link key={ route.name } to={ route.path }>{ route.name }</Link>
-                  : (
-                    <>
-                      <div>{ route.name }</div>
-                      {
-                        route.children.map((childrenRoute, index) => {
-                          return <li><Link key={ route.path + childrenRoute.path } to={ route.path + childrenRoute.path } >{ childrenRoute.name }</Link></li>;
-                        })
-                      }
-                    </>
-                  )
-        })
-      }
-    </MainDiv>
+    <>
+      <h1>연습장</h1>
+      <MainContainer>
+        {
+          routes.map(route => {
+            return !route.children
+                    ? (
+                        <Card>
+                          <Link key={ route.name } to={ route.path }>{ route.name }</Link>
+                        </Card>
+                      )
+                    : (
+                        <Card>
+                          { route.name }
+                          <ul>
+                            {
+                              route.children.map((childrenRoute, index) => {
+                                return <li><Link key={ route.path + childrenRoute.path } to={ route.path + childrenRoute.path } >{ childrenRoute.name }</Link></li>
+                              })
+                            }
+                          </ul>
+                        </Card>
+                      )
+          })
+        }
+      </MainContainer>
+    </>
   )
 }
 
