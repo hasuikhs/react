14bae9559e3dc29e0176d0f99d2a4421cd278f45
hanseok kim <hasuikhs@gmail.com>
2023-02-09 22:08:05 +0900

    230209|Update MutationObserver use

--- demo/src/pages/Window.js
+++ demo/src/pages/Window.js
@@ -1,11 +1,13 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useLocalStorage } from '../hooks/useStorage';
 import useMutationObserver from '../hooks/useMutaionObserver';
 
 function Window() {
-  const [local, setLocal] = useLocalStorage({ key: 'test' })
+  const [local, setLocal] = useLocalStorage({ key: 'test' });
   const inputRef = useRef();
   const mutationRef = useRef();
+
+  const [placeholder, setPlaceholder] = useState('기본');
 
   const test = () => {
     console.log('test')
@@ -19,7 +21,11 @@ function Window() {
     window.addEventListener('resize', test);
 
     setTimeout(() => {
-      mutationRef.current.placeholder = '변경';
+      // mutationRef.current.placeholder = '변경';
+      
+      // state를 이용한 옵저버는 적용되지 않음
+      // state가 변경될 때는 컴포넌트 자체가 재랜더링 됨
+      setPlaceholder('변경');
     }, 3_000);
 
 
@@ -43,7 +49,7 @@ function Window() {
   return (
     <>
       <input type="text" ref={ inputRef }onChange={ onChange } />
-      <input type="text" placeholder="" ref={ mutationRef } />
+      <input type="text" placeholder={ placeholder } ref={ mutationRef } />
     </>
   )
 }
