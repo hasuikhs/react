bc5ff6d4024ae689dabbb5dcbec87d6d86f5aa54
hanseok kim <hasuikhs@gmail.com>
2024-05-06 19:55:26 +0900

    240506|Update nextjs14

---
+++ learn-nextJS14/app/(movies)/movies/[id]/page.tsx
@@ -0,0 +1,22 @@
+import { Suspense } from "react";
+
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
+
+export default async function MovieDetail({
+  params: { id }, searchParams
+}: {
+  params: { id: string }, searchParams: URLSearchParams
+}) {
+
+  return (
+    <div>
+      <Suspense fallback={ <h1>Loading movie info</h1> }>
+        <MovieInfo id={ id }/>
+      </Suspense>
+      <Suspense fallback={ <h1>Loading movie videos</h1> }>
+        <MovieVideos id={ id } />
+      </Suspense>
+    </div>
+  );
+}
--- learn-nextJS14/app/about-us/page.tsx
+++ learn-nextJS14/app/about-us/page.tsx
@@ -1,9 +1,10 @@
-import Navigation from "../../components/navigation";
+export const metadata = {
+  title: 'AboutUs'
+};
 
 export default function AboutUs() {
   return (
     <div>
-      <Navigation/>
       <h1>About Us!</h1>
     </div>
   );
